76ce165c7f41e1b95ad02917ea8d73150ec78630
Schmidt <sabrina.schmidt@training.lug-ag.de>
2025-04-07 15:53:53 +0200

    cards

--- Algoriithms/Program.cs
+++ Algoriithms/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ int[] cards = [ 3,5,7,8,9];
+ 
+ int query = 7;
+
+ var result = FindCardLocation(cards, query);
+ System.Console.WriteLine(result);
+
+int FindCardLocation(int [] cards, int query)
+{
+    /* 
+    Definiere Variable "index" und initialisiere mit Wert 0
+    Wiederhole bis "index" gleich "cards" length:
+        Wenn "cards" an "index" gleich "query", 
+        dann:
+            Gebe "index" zurück
+    Inkrementiere "index" um 1
+    */
+    int index = 0;
+    while (index < cards.Length)
+    {
+        if (cards[index] == query)
+            return index;
+            index++;
+    }
+    return -1;
+}
+
