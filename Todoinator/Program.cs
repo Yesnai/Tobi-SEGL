7c7ca9b0e68295e3cee1966641182ad505f4561a
Schmidt <sabrina.schmidt@training.lug-ag.de>
2025-04-09 14:36:00 +0200

    Todoinator

--- Todoinator/Program.cs
+++ Todoinator/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+var todoList = new List<TodoItem>();
+Console.WriteLine(value:"Willkommen beim Todoinator!");
+while (true)   
+{
+    foreach(var item in todoList)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine(value:"""
+    1.Neues Todo
+    2.Todo abhaken
+    """);
+    var userMenuChoice= Console.ReadLine();
+    switch (userMenuChoice)
+    {
+        case "1":
+            //Todo erstelllen
+            Console.WriteLine("Was ist das Todo?");
+            var todoDescription = Console.ReadLine();
+            Console.WriteLine("""
+            Wie wichtig? 
+            1 => Hohe Prio
+            2 => Normale Prio
+            3 => Niedrige Prio
+            """);
+            var todoPrio = Console.ReadLine();
+            var newTodo = new TodoItem();
+            newTodo.Description= todoDescription;
+            newTodo.Prio = Convert.ToInt32 (todoPrio);
+            todoList.Add(newTodo);
+            break;
+        case "2":
+            //Todo abhaken
+            System.Console.WriteLine("WElches Todo möchteset du löschen?");
+            var indexToDelete = Convert.ToInt32(Console.ReadLine());
+            todoList.RemoveAt(indexToDelete);
+            break;
+        default:
+            //Nicht definierter Input eg. "hamester"
+            break;
+
+    }
+    Console.Clear();
+}
--- Todoinator/TodoItem.cs
+++ Todoinator/TodoItem.cs
@@ -1,8 +1,18 @@
-using System;
-
-namespace Todoinator;
+// Klasse => Eigenener Datentyp
 
 public class TodoItem
 {
+    // Klassen definieren Variablen (Daten) und Methoden (Funktionalität)
+    // Variablen können Delder (Fields) oder EIgenschaften (Properties) sein
+    // Felder sind privat -> KLassenintern
+    // Eigenschaften sind public -> Kann von außen 
 
+    public string Description { get; set; }
+    public int Prio { get; set; }
+   
+    // Instanz-Methode => myTodo.ToString();    
+    public override string ToString()
+    {
+        return $"{Description} - {Prio}";
+    }
 }
